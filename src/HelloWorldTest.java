f3723714ce1ee502f76f97ac46fea387bebb1f00
Marten Tamm <marten.tamm@gmail.com>
2019-03-02 12:25:59 +0200

    confliict

--- src/HelloWorldTest.java
+++ src/HelloWorldTest.java
@@ -1,5 +1,5 @@
 public class HelloWorldTest {
     public static void main(String[] args) {
-        System.out.println("hello conflict!");
+        System.out.println("hello conflict!1!!!");
     }
 }
